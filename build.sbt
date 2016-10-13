87c225ce6127b93eab4f4436baf53dfec1bbc5c9
jvican <jorgevc@fastmail.es>
2016-10-13 15:51:41 +0200

    Add Hello World test

--- build.sbt
+++ build.sbt
@@ -4,3 +4,4 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
     
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.0" % "test"
---
+++ src/test/scala/HelloWorld.scala
@@ -0,0 +1,9 @@
+package helloworld
+
+import org.scalatest.FunSuite
+
+class HelloWorld extends FunSuite {
+  test("Print 'Hello world!'") {
+    println("Hello World!")
+  }
+}
